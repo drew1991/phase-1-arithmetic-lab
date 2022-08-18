992cbc69d73d8eb1358535a33a945534cab98012
Andrew Onyancha <mr.onyanchaandrew@gmail.com>
2022-08-18 18:12:31 +0300

    completed message

--- index.js
+++ index.js
@@ -3,7 +3,7 @@
 let num1 = 2
 let num2 = 31
 let multiply = num1*num2
-let random=Math.floor(Math.random()) + 1;
+let random=Math.floor(Math.random()>0) ;
 let num3=34
 let num4=5
 let mod = num3%num4
